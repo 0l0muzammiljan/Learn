4ce72d190d970e477c13e879e579bc72971144b2
Muzammil Jan <0l0muzammiljan@gmail.com>
2024-08-01 22:47:12 +0000

    Revert "added Temperature.py"
    This reverts commit b4434faf729dbbf915ab057a0196e5b34ae554b0.

--- Temperature.py
+++
@@ -1,7 +0,0 @@
-celsius =float
-fahrenheit = float
-celsius = float(input("Enter temperature in Celsius: "))
-
-fahrenheit = (1.8 * celsius) + 32
-
-print("The temperature in Fahrenheit is: " + str(round(fahrenheit, 2)))
